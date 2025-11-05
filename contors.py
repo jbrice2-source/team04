43505ca0a8de983582694510c3a7eaf32760d04a
Sean Hann <shann1@sheffield.ac.uk>
2025-11-05 17:57:02 +0000

    improved contours algorithm for automatic image preprocessing

--- contors.py
+++ contors.py
@@ -1,55 +1,85 @@
 import numpy as np
 import cv2
-img = cv2.imread('pictures/0_0.png')
+from glob import glob
+
+images = glob("pictures/*.png")
+
+for n in images[:]:
+    original_img = cv2.imread(n)
+    img = original_img.copy()
+
+    # imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((3, 3), np.uint8)
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # equalised_image = cv2.equalizeHist(imgray)
+    # equalised_image = cv2.cvtColor(equalised_image, cv2.COLOR_GRAY2BGR)
+    
+    # img = cv2.add(img,equalised_image)
+    # background = cv2.dilate(img,kernel,iterations=1)
+    # img = cv2.subtract(background, img)
+    # cv2.imshow("subtract", img)
+
+    
+    blur = cv2.bilateralFilter(img, 11, 75,75)
+    cv2.imshow("bilatteral filter", blur)
+
+    median = np.median(np.mean(original_img,axis=2))
+    lower_threshold = int(max(0, 0.66 * median))
+    upper_threshold = int(min(255, 1.33 * median))
+    # blur = cv2.copyMakeBorder(blur,2,2,2,2,cv2.BORDER_CONSTANT,value=[255, 255, 255])
+
+    edges = cv2.Canny(blur,lower_threshold,upper_threshold)
 
 
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(imgray,175,200)
-ret, thresh = cv2.threshold(edges, 127, 255, 0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    edges = cv2.dilate(edges,kernel,iterations=2)
+    edges = cv2.erode(edges,kernel,iterations=2)
+    edges = cv2.bitwise_not(edges)
 
-cont_list = []
-hiere_list = []
-for i,n in enumerate(contours):
-    if len(n) < 100:
-        cont_list.append(n)
-        hiere_list.append(hierarchy[0,i])
+    cv2.imshow("processed edges", edges)
 
-# print(hierarchy)
-# print(hierarchy[0,:,3])
+    ret, thresh = cv2.threshold(edges, 200, 255, 0,cv2.THRESH_BINARY)
+    print(thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# child_map = {}
-# counter = (-1,0)
-# for i in hierarchy[0,:,3][::-1]:
-#     if i != -1:
-#         counter = (i,counter[1]+1)
-#     elif counter[0] != -1:
-#         child_map.update({counter[0]:counter[1]})
-#         counter = (-1,0)
-#     else:
-#         counter = (-1,0)
+    child_num = np.zeros(hierarchy.shape[1])
+    for i in range(len(child_num)-1,-1,-1):
+        if hierarchy[0,i,3] != -1:
+            child_num[hierarchy[0,i,3]] += 1 + child_num[i]
+            
+    # print(child_num)
+    # print(hierarchy)
+    cont_list = []
+    hiere_list = np.array([])
+    for i,n in enumerate(contours):
+        if len(n) < 500:
+            cont_list.append(n)
+            hiere_list = np.append(hiere_list,hierarchy[0,i]).reshape(-1,4)
 
+    print(list(map(len,contours)))
+    print(list(map(len,cont_list)))
 
-# print(child_map)
-# item_max = (-1,0)
-# for i in child_map.items():
-#     if item_max[1] <= i[1]:
-#         item_max = i
+    img2 = blur.copy()
+    cv2.drawContours(img2, contours, -1, (0,255,0), 1)
+    cv2.imshow('Contours raw', img2)
+    # cv2.waitKey(0)
 
-print(list(map(len,cont_list)))
-print(np.max(cont_list))
-# print(item_max)
-for i in range(0,len(cont_list)):
-    img2 = img.copy()
-    cv2.drawContours(img2, cont_list[i:], -1, (0,0,255), 1)
+    # for i in cont_list:
+    img3 = original_img.copy()
+    cv2.drawContours(img3, cont_list, -1, (0,0,255), 1)
+    cv2.imshow('Contours', img3)
+    # cv2.waitKey(0)
 
-    # cv2.drawContours(img, contours, 3, (0,255,0), 3)
+    cont_max = np.array(list(map(lambda x:np.max(x,axis=0),cont_list))).reshape(-1,2)
+    cont_min = np.array(list(map(lambda x:np.min(x,axis=0),cont_list))).reshape(-1,2)
+    # print(cont_min)
+    print(cont_min[:,0].min(),cont_max[:,0].max(),cont_min[:,1].max(),cont_max[:,1].max())
+    cv2.imshow('Contours', img3)
+    # cv2.waitKey(0)
 
-    # cnt = contours[4]
-    # cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
-
-    cv2.crop()
-
-    cv2.imshow('Contours', img2)
+    cv2.imshow("Contours crop",img3[max(cont_min[:,1].min()-30,0):min(cont_max[:,1].max()+30,img3.shape[1]),
+                                    max(cont_min[:,0].min()-30,0):min(cont_max[:,0].max()+30,img3.shape[1])])
     cv2.waitKey(0)
+
 cv2.destroyAllWindows()
